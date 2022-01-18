76fc25691331ca3a1f896cb47c8390020c3a234f
JFJoao <96094151+JFJoao@users.noreply.github.com>
2022-01-17 23:46:05 -0300

    Create FilaPrioridade.py

---
+++ FilaPrioridade.py
@@ -0,0 +1,11 @@
+nome = input("digite seu nome:")
+idade = int(input("digite sua idade: "))
+doenca_infectocontagiosa = input("O paciente tem suspeita de doenca infectocontagiosa? ").upper()
+if idade >= 65:
+    print("O paciente " + nome + " tem prioridade no atendimento! ")
+elif doenca_infectocontagiosa == "Sim":
+    print ("O paciente "+nome+" deve aguardar na sala de espera reservada")
+else:
+    print("O paciente " + nome + " esta cadastrado, favor aguardar.")
+    if idade <= 10:
+        print("Paciente "+nome + " é uma criança."
